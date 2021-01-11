a3b7fcdda77431cb46ec53bec4d5f6570b649838
Andrew Rondeau <git@andrewrondeau.com>
2021-01-11 11:05:15 -0500

    First pass at obfuscation

--- src/index.js
+++ src/index.js
@@ -1,92 +1,43 @@
-// Solution for https://adventofcode.com/2019/day/2
+// Obfuscated solution for https://adventofcode.com/2019/day/2
 // (C)opyright 2021 Andrew Rondeau
 
-// Opcodes:
-const ADD = 1;
-const MULTIPLY = 2;
-const END = 99;
+const heap = JSON.parse(`[${process.argv[2]}]`);
 
-const argv = process.argv;
-
-if (argv.length != 3) {
-    console.log('Please provide the input as a command-line argument. See https://adventofcode.com/2019/day/2');
-    return;
-}
-
-// A comma-separated list of numbers easily converts to JSON. I'm going to do minimal input checking and rely on
-// JSON errors
-const inputString = argv[2];
-const inputJson = `[${inputString}]`;
-
-var heap;
-try {
-    heap = JSON.parse(inputJson);
-} catch (error) {
-    console.error(`Can not parse input: "${inputString}": ${error}`);
-    console.error(`(Attempted to convert to JSON as "${inputJson}")`);
-    return;
-}
-
-var instructionPointer = 0;
+var instructionPointer = -1;
 
 // Performs an operand on the heap
 const operate = (operation) => {
-    const aAddress = heap[instructionPointer + 1];
-    const bAddress = heap[instructionPointer + 2];
-    const destinationAddress = heap[instructionPointer + 3];
 
-    // It's undefined when these values are out of the range of the heap
-    // For now I'm just letting the Javascript exception bubble out
-    const a = heap[aAddress];
-    const b = heap[bAddress];
+    const a = heap[heap[++instructionPointer]];
+    const b = heap[heap[++instructionPointer]];
 
-    // It's undefined what happens when destinationAddress is out of range
-    // For now I'm just going to let the heap grow if an address is out of range
-    // (or negative)
-
-    result = operation(a, b);
-    heap[destinationAddress] = result;
-
-    instructionPointer += 4;
+    heap[heap[++instructionPointer]] = operation(a, b);
 };
 
-var running = true;
+while (true) {
+    var opCode = heap[++instructionPointer];
 
-try {
-    while (running) {
-        var opCode = heap[instructionPointer];
+    // Note that I'm just letting array bounds issues fall through as exceptions
+    // In industrial-strength code, I'd be much more careful about bounds checking, and
+    // give more detailed errors
 
-        // Note that I'm just letting array bounds issues fall through as exceptions
-        // In industrial-strength code, I'd be much more careful about bounds checking, and
-        // give more detailed errors
+    switch (opCode) {
+        case 1:
+            operate((a, b) => a + b);
+            break;
 
-        switch (opCode) {
-            case ADD:
-                operate((a, b) => a + b);
-                break;
+        case 2:
+            operate((a, b) => a * b);
+            break;
 
-            case MULTIPLY:
-                operate((a, b) => a * b);
-                break;
+        case 99:
+            const resultJSON = JSON.stringify(heap);
+            const resultString = resultJSON.substring(1, resultJSON.length - 1);
+            console.log(resultString);
+            
+            return;
 
-            case END:
-                running = false;
-                break;
-
-            default:
-                console.error(`Unexpected opCode at ${instructionPointer}: ${opCode}`);
-                running = false;
-
-                break;
-        } 
-    }
-} catch (error) {
-    console.error(`Error running the program: ${error}`);
-    return;
+        default:
+            throw `Unexpected opCode at ${instructionPointer}: ${opCode}`;
+    } 
 }
-
-// Success, program complete
-const resultJSON = JSON.stringify(heap);
-// Get rid of [...]
-const resultString = resultJSON.substring(1, resultJSON.length - 1);
-console.log(resultString);
